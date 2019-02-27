87d4eaff8324c09f416ef12b6f62cc3eee52840c
Albert Nisbet <albertnis@gmail.com>
2019-02-27 23:09:39 +1300

    Move MIME type list into sls adapter

--- src/backend/serverlessAdapter.js
+++ src/backend/serverlessAdapter.js
@@ -1,5 +1,24 @@
 const sls = require("serverless-http")
-const binaryMimeTypes = require("./binaryMimeTypes")
+
+const binaryMimeTypes = [
+  'application/javascript',
+  'application/json',
+  'application/octet-stream',
+  'application/xml',
+  'font/eot',
+  'font/opentype',
+  'font/otf',
+  'image/jpeg',
+  'image/png',
+  'image/svg+xml',
+  'text/comma-separated-values',
+  'text/css',
+  'text/html',
+  'text/javascript',
+  'text/plain',
+  'text/text',
+  'text/xml'
+]
 
 const server = require("./backend")
 module.exports.server = sls(server, {
